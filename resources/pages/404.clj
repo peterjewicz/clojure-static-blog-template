722dabfcf0602c3b04623fcd297e960ecb7cdc09
Peter Jewicz <Peterjewicz@totalwebconnections.com>
2021-03-18 15:13:10 -0500

    add basic 404

---
+++ resources/pages/404.clj
@@ -0,0 +1,10 @@
+; This page is used to style a 404 page for your site - accessible at /404
+; You will have to direct your host to point 404 requests on your domain to this page.
+
+
+{:header {
+          :title "Not Found"
+          :meta-desc "This is a page for 404 handling"}
+ :page [:div.Page
+         [:h1 "404 - Page Not Found"]
+         [:p "Use this page to create a 404 page"]]}
